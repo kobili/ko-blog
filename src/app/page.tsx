9ab66f2a8862b54bb0db7b15fe3332c79df678f5
Kobe Li <likobe23@gmail.com>
2023-11-23 20:57:47 -0800

    Simplify things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Layout from '@/components/Layout';
 import { Metadata } from 'next';
 import Link from 'next/link';
 
@@ -11,14 +10,10 @@ export const metadata: Metadata = {
 
 export default function Home() {
     return (
-        <Layout>
-            <div className='flex flex-col items-center'>
-                <h1 className='text-9xl my-9 text-slate-400 drop-shadow-md'>Kobe Li</h1>
-                <Link href="/about" className='text-xl text-blue-400 drop-shadow-sm'>About Me</Link>
-                <Link href="/blog/EB-https" className='text-xl text-blue-400 drop-shadow-sm'>Setting Up HTTPS on an AWS Elastic Beanstalk Express App</Link>
-            </div>
-
-
-        </Layout>
+        <div className='flex flex-col items-center'>
+            <h1 className='text-9xl my-9 text-slate-400 drop-shadow-md'>Kobe Li</h1>
+            <Link href="/about" className='text-xl text-blue-400 drop-shadow-sm'>About Me</Link>
+            <Link href="/blog/EB-https" className='text-xl text-blue-400 drop-shadow-sm'>Setting Up HTTPS on an AWS Elastic Beanstalk Express App</Link>
+        </div>
     )
 }
